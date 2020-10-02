d4ca916b356115af2388c1ea84282a7fabec569a
yesenia.torres3 <yesenia.torres3@nordstorm.com>
2020-10-02 13:22:34 -0700

    passing all tests

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,33 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return nil if nums.nil? || nums.length == 0
     
-    raise NotImplementedError, "Method not implemented yet!"
+    # initialize 
+    max_so_far = 0
+    max_ending_here = 0
+    i = 0 
+
+    # handle all negatives array case 
+    if nums.none? { |num| num.positive? }
+        return nums.max
+    else 
+        # apply Kadane’s Algorithm
+        while i < nums.length 
+            max_ending_here = max_ending_here + nums[i]
+    
+            if max_ending_here < 0 
+                max_ending_here = 0
+            end 
+    
+            if max_so_far < max_ending_here
+                max_so_far = max_ending_here
+            end 
+    
+            i += 1
+        end 
+    end 
+
+    return max_so_far
 end
