8ac250c484108a16d7a0bd88dc5014967a415211
quinqu <quinruby0@gmail.com>
2020-10-01 18:29:41 -0700

    algorithms

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,21 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: (1)
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    answ = ""
+    current_max = nums[0]
+    global_max = nums[0]
+
+    for i in 0...nums.length 
+        if current_max < nums[i]
+            current_max = nums[i]
+        elsif nums[i] < current_max
+            current_max = current_max + nums[i]
+        end 
+        if current_max > global_max
+            global_max = current_max
+        end 
+    end 
+    return global_max
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,28 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  if num <= 0
+    raise ArgumentError
+  elsif num == 1
+    return "1"
+  elsif num == 2
+    return "1 1"
+  end 
+
+  storage = [0, 1, 1]
+  answ = "1 1"
+  curr = 3
+  
+  while curr < num + 1
+    f = storage[curr - 1]
+    s = curr - storage[curr - 1]
+    storage[curr] =  storage[f] + storage[s]
+    answ += " " + storage[curr].to_s
+    curr += 1 
+  end 
+  return answ
 end
+
+
