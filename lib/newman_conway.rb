232ef0cb0d4c6feddb4fc40ad6893036f4fa73b7
Jessica - Ada <jessica-ada@osxltTML86.local>
2020-10-02 16:54:29 -0700

    1

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  arr = Array.new(num, 0)
+
+  n = 0
+  while n <= num
+    if n == 1 || n == 2
+      arr[n] = 1
+    else
+      arr[n] = arr[arr[n - 1]] + arr[n - arr[n - 1]]
+    end
+
+    n += 1
+  end
+
+  arr[1..(arr.length - 1)].join(' ')
 end
