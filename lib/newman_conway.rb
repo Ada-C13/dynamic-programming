50645ac5f452d88feba7905cfcbcfc0b66039bfb
cojenco <icathyouyang@gmail.com>
2020-10-02 21:13:30 -0700

    revise newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,24 +1,34 @@
-
-
 # Time complexity: O(n) and actually O(3n) since I'm using shift and join? 
-# Space Complexity: ?
+# Space Complexity: O(n) for the additional memo array storing n of the sequence
 # P(n) = P(P(n - 1)) + P(n - P(n - 1))
-# P(5) = P(P(4)) + P(5 - P(4))
-#      = P(2) + P(3) = 3
 def newman_conway(num)
   raise ArgumentError.new("num must be a positive integer") if num <= 0
   return "1" if num == 1
-  return "1 1" if num == 2
 
-  memo = ["", 1, 1]
+  # n starts at 3 per newman_conway property, though all indice in memo[] need to deduct by 1
+  memo = [1, 1]
   n = 3
   while n <= num
-    pre = memo[n-1]
-    res = memo[pre] + memo[n - pre] 
-    memo[n] = res
+    pre = memo[n - 1 - 1]                               # pre = P(n-1)
+    memo[n - 1] = memo[pre - 1] + memo[n - pre - 1]     # P(n) stored at index (n-1) = P(P(n - 1)) + P(n - P(n - 1)) 
     n += 1
   end
 
-  memo.shift
   return memo.join(" ")
 end
+
+
+# alternative solution w/o shifting indice
+# def newman_conway(num)
+#   raise ArgumentError.new("num must be a positive integer") if num <= 0
+#   return "1" if num == 1
+#   memo = ["", 1, 1]
+#   n = 3
+#   while n <= num
+#     pre = memo[n-1]
+#     memo[n] = memo[pre] + memo[n - pre] 
+#     n += 1
+#   end
+#   memo.shift
+#   return memo.join(" ")
+# end
