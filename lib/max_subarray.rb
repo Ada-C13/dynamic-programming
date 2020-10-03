ab221c8ed2e5935298a48ea7b11a0dcc97fc285b
Jessica - Ada <jessica-ada@osxltTML86.local>
2020-10-02 17:49:43 -0700

    2

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,24 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1), no new structures created
 def max_sub_array(nums)
-    return 0 if nums == nil
+  return nil if nums.empty?
+  return 0 if nums == nil
     
-    raise NotImplementedError, "Method not implemented yet!"
+  max_so_far = nums[0]
+  max_ending_here = nums[0]
+
+  nums.each do |num|
+    if max_ending_here < num
+      max_ending_here = num
+    elsif num < max_ending_here
+      max_ending_here += num
+    end
+
+    if max_so_far < max_ending_here
+      max_so_far = max_ending_here
+    end
+  end
+
+  return max_so_far
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -6,15 +6,12 @@ def newman_conway(num)
   raise ArgumentError if num == 0
   arr = Array.new(num, 0)
 
-  n = 0
-  while n <= num
+  (num + 1).times do |n|
     if n == 1 || n == 2
       arr[n] = 1
     else
       arr[n] = arr[arr[n - 1]] + arr[n - arr[n - 1]]
     end
-
-    n += 1
   end
 
   arr[1..(arr.length - 1)].join(' ')
