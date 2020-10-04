6f003515b8f7b5bdba1528c544eee04e0950b4b5
dnguye2 <dnguyen1093@gmail.com>
2020-10-04 12:54:05 -0700

    completed implementation of Kadane's algorithm

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,18 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
+    return nil if nums.empty?
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    return nums[0] if nums.length == 1
+
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    for num in nums[1..-1] do
+        max_ending_here = [num, (max_ending_here + num)].max
+        max_so_far = [max_so_far, max_ending_here].max
+    end
+
+    return max_so_far
 end
