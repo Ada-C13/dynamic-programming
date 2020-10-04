4265d3bc14416867baa7fe08f59d33c8ea71cb42
tithvorlakmok <tithvorlakmok@gmail.com>
2020-10-04 13:27:09 -0700

    Passed all the tests

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,22 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
+# resource: https://www.geeksforgeeks.org/print-n-terms-newman-conway-sequence/
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+  arr = []
+  arr[0] = 0
+  arr[1] = 1
+  arr[2] = 1
+  result = "1 1"
+  i = 3
+  while i <= num
+    arr[i] = arr[arr[i - 1]] + arr[i - arr[i - 1]]
+    result += " #{arr[i]}"
+    i += 1
+  end
+  return result
 end
