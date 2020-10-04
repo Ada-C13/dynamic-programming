a21cf0acc27acd6b254bfce46d0f7179de40e230
dnguye2 <dnguyen1093@gmail.com>
2020-10-03 23:15:14 -0700

    completed newman-conway sequence function

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,23 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
-end
+  raise ArgumentError, "num must be > 0" if num <= 0
+
+  return '1' if num == 1
+  return '1 1' if num == 2
+
+  # build a memo of subproblems
+  # fill in the base case
+  seq = [0, 1, 1]
+  
+  # solve all subproblems
+  n = 2
+  for n in (3..num) do
+    seq.push(seq[seq[n-1]] + seq[n-seq[n-1]])
+    n += 1
+  end
+
+  return seq[1..num].join(' ')
+end 
