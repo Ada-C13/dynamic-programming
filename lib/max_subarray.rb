2ca58c4cbe16c5ca3a16580cf930e6a101131b42
Sharon Cheung <keikei1128@g.ucla.edu>
2020-10-03 19:20:38 -0700

    max_subarray tests passed

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,6 +3,15 @@
 # Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+    max_current = max_sum = nums[0]
+
+    i = 1
+    while i <= (nums.length - 1)
+        max_current = [nums[i], (max_current + nums[i])].max
+        if max_current > max_sum
+            max_sum = max_current
+        end
+        i += 1
+    end
+    return max_sum
 end
