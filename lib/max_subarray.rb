2c6d399175647734c6f0f61d30e9454f4f0b1277
cojenco <icathyouyang@gmail.com>
2020-10-04 15:56:40 -0700

    complete max_subarray

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,20 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def max_sub_array(nums)
-    return 0 if nums == nil
+    return nil if nums.nil? || nums.empty?
     
-    raise NotImplementedError, "Method not implemented yet!"
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    i = 1
+    while i < nums.length
+      max_ending_here += nums[i]
+      # assign max to current num if previous accumulated is deducting value (~liability)
+      max_ending_here = nums[i] if max_ending_here < nums[i]
+      max_so_far = max_ending_here if max_so_far < max_ending_here
+      i += 1
+    end
+
+    return max_so_far
 end
