b5ef9c9a149058d8879c7eef3ac24184f2f60508
Leah <leahwhughes@gmail.com>
2020-10-05 07:55:47 -0700

    adds n-c & max sub array

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,8 +1,28 @@
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) - n is the length of the array
+# Space Complexity: O(1) - not creating additional space in memory
 def max_sub_array(nums)
-    return 0 if nums == nil
+  return nil if nums.empty?
+  return nums[0] if nums.length == 1
     
-    raise NotImplementedError, "Method not implemented yet!"
+  total = nums[0] # represents the current sum in question
+  max = nums[0] # represents the maximum we've seen so far
+    
+  i = 1
+  while (i < nums.length)
+    if (total + nums[i]) > nums[i] # if current sum plus current number is greater than the current number
+    total += nums[i] # add it to the sum
+      if max < total # if the max_so_far is less than the current_total
+        max = total # max is equal to the total
+      end
+    else # the current sum plus the current number is less than or equal to the current number
+      total = nums[i] # current total equals the current number
+      if max < nums[i] # if the maximum is less than the current number
+        max = nums[i] # max is the current number
+      end
+    end
+    i += 1
+  end
+    
+  return max
 end
--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,17 @@
 
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  return "1" if num == 1
+  
+  arr = [0,1,1]
+  
+  i = 3
+  while i <= num
+    arr << arr[arr[i - 1]] + arr[i - arr[i - 1]]
+    i += 1
+  end
+
+  return arr[1..-1].join(' ')
 end
