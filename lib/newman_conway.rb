5ed62cd9d3d3daae20611a9f4be5aba6f20db37f
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-10-04 22:22:32 -0700

    added newman_conway method

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: 0(n)?
+# Space Complexity: 0(n)?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num == 0
+  array = []
+
+  array.push(1)
+  return array.join(" ") if num == 1
+  
+  array.push(1)
+  return array.join(" ") if num == 2
+
+  i = 2
+  while i < num
+    array[i] = array[array[i-1]-1] + array[i - array[i-1]]
+    i += 1
+  end
+ return array.join(" ")
 end
