d984a24dd90a19c9d7c25c5cb159619100f81020
Hannah <hannahjoo24@gmail.com>
2020-10-04 19:25:06 -0700

    Update max_subarray and newman_conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
-def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+# Time complexity: O(n) => because I iterate depending on the input (num)
+# Space Complexity: O(n) => because I create a new array depending on the input (num)
+
+def newman_conway(num) # 9
+  raise ArgumentError if num == 0 
+  solution = []
+  
+  (1..num).each do |n|
+    if n == 1 || n == 2
+      solution[n] = 1 
+    else 
+      # P(P(n - 1)) + P(n - P(n - 1))
+      solution[n] = solution[solution[n - 1]] + solution[n - solution[n - 1]]
+    end 
+
+  end 
+
+  return solution[1..-1].join(" ")
 end
