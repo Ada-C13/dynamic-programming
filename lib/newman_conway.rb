d6184428bef17ff871a0963f30b3a61a8e5eacea
peachmakkoli <lee.higgins.dev@gmail.com>
2020-10-06 20:19:56 -0700

    completes newman_conway method

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,21 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n) if num is greater than 2, where n is num 
+# Space Complexity: O(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num < 1
+  sequence = [0, 1, 1]
+  output = ""
+  output += "#{sequence[1]}" + " "
+  output += "#{sequence[2]}" + " " if num > 1
+
+  i = 3
+  while i <= num
+    sequence << sequence[sequence[i - 1]] + sequence[i - sequence[i - 1]]
+    output += "#{sequence[i]}" + " "
+    i += 1
+  end
+
+  return output.chomp(" ")
 end
+        
