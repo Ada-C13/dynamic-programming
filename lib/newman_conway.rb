4b5932dd67d1e97a5533bb89b88d7b35c66d0f04
Haben Foto (Third Sector New England Inc) <v-habenfoto@microsoft.com>
2020-10-08 22:06:52 -0700

    added new_conway method

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,19 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  new_array = [0, 1, 1]
+  i = 3
+
+  while i <= num
+    new_array[i] = new_array[new_array[i - 1]] + new_array[i - new_array[i - 1]]
+    
+    index += 1
+  end
+  
+  return new_array(1..-1)
+
 end
