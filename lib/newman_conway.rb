f85a76957c2e4e8fd96155b8a54041bf19926efc
Jessica Stone <jstone4815@gmail.com>
2020-10-11 20:06:01 -0700

    implement newman-conway algorithm

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,14 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+  
+  arr = [0, 1, 1]
+  while arr.length <= num
+    arr.push(arr[arr[arr.length - 1]] + arr[arr.length - arr[arr.length - 1]])
+  end
+
+  return arr[1..arr.length].join(" ")
 end
