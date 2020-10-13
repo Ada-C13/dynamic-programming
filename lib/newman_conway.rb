c77ba286c23bbe875d1f60fdaab6ad6ed6942a87
kate.m <kate.mangubat@highspot.com>
2020-10-13 12:02:10 -0700

    dynamic algos

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,24 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: ? o(n)
+# Space Complexity: ? o(n)
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  return 1 if num == 1
+
+  newman = [0, 1, 1]
+  goal = "1 1"
+  i = 3
+
+  #P(n) = P(P(n - 1)) + P(n - P(n - 1)) 
+  #https://www.geeksforgeeks.org/newman-conway-sequence/
+  until i >= num + 1
+    current_newman = newman[newman[i - 1]] + newman[i - (newman[i - 1])]
+  
+    newman << current_newman
+    goal += " #{current_newman}"
+    i += 1
+  end
+
+  return goal
 end
