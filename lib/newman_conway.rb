816ca0ff860e3d0b61c58b3eb6ce34838e4b8751
mulhoo <oliviamulholland94@gmail.com>
2020-10-16 17:14:33 -0700

    newman_conway complete

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,7 +1,20 @@
-
-
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  starting_array = [0, 1, 1] 
+
+  result = "1"
+  count = 3
+
+  while count <= num
+    starting_array[count] = starting_array[starting_array[count - 1]] + starting_array[count - starting_array[count - 1]]
+    count += 1
+  end
+  
+  (2..num).each do |i|
+    result += " #{starting_array[i]}"
+  end
+  
+  return result
 end
