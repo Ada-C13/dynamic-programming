6895d2bf1ee321ba796c15f0d42cb55f70b4e76c
Haben <haben7@gmail.com>
2020-11-17 11:16:52 -0800

    update max_sub_array method

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -1,20 +1,18 @@
+# frozen_string_literal: true
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 def max_sub_array(nums)
-    return 0 if nums == nil
+  return nil if nums.nil? || nums.empty?
 
-    max_so_far = nums[0]
-    curr_max = nums[0]
-    arr_size = nums.size
+  max_so_far = nums[0]
+  curr_max = nums[0]
 
-    for i in 1...arr_size
-        curr_max = (nums[i] > curr_max+nums[i]) ? nums[i] : curr_max+nums[i]
-        max_so_far = (max_so_far > curr_max) ? max_so_far : curr_max
-    end
+  nums[1..].each do |curr_num|
+    curr_max = [curr_num, curr_num + curr_max].max
+    max_so_far = [curr_max, max_so_far].max
+  end
 
-    max_so_far
-
+  max_so_far
 end
-
-
