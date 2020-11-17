9c40385a063af4a7c30cf2630bc5d8cd27d3fa26
Haben <haben7@gmail.com>
2020-11-17 11:17:12 -0800

    add time and space complexity

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -1,21 +1,20 @@
-
+# frozen_string_literal: true
 
 # Time complexity: O(n)
 # Space Complexity: O(n)
 def newman_conway(num)
   raise ArgumentError if num <= 0
-  return "1" if num == 1
-  return "1 1" if num == 2
+  return '1' if num == 1
+  return '1 1' if num == 2
 
   new_array = [0, 1, 1]
   i = 3
-
+  # P(P(n - 1)) + P(n - P(n - 1))
   while i <= num
     new_array[i] = new_array[new_array[i - 1]] + new_array[i - new_array[i - 1]]
-    
+
     i += 1
   end
 
-  return new_array[1..-1].join(" ")
-
+  new_array[1..].join(' ')
 end
