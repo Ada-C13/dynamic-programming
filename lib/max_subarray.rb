b4ec538f3bc989d5da68c050e110331dca2a78ec
Haben <haben7@gmail.com>
2020-11-17 10:54:06 -0800

    add max_sub_array method

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,6 +3,18 @@
 # Space Complexity: ?
 def max_sub_array(nums)
     return 0 if nums == nil
-    
-    raise NotImplementedError, "Method not implemented yet!"
+
+    max_so_far = nums[0]
+    curr_max = nums[0]
+    arr_size = nums.size
+
+    for i in 1...arr_size
+        curr_max = (nums[i] > curr_max+nums[i]) ? nums[i] : curr_max+nums[i]
+        max_so_far = (max_so_far > curr_max) ? max_so_far : curr_max
+    end
+
+    max_so_far
+
 end
+
+
