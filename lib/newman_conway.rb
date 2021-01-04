06e00b330e7f49d87efb9e70e9596832b4ffc06d
Stephanie <stephaniejmars@gmail.com>
2021-01-03 18:57:18 -0800

    newman conway

--- lib/newman_conway.rb
+++ lib/newman_conway.rb
@@ -3,5 +3,20 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num)
-  raise NotImplementedError, "newman_conway isn't implemented"
+  raise ArgumentError if num <= 0
+  return "1" if num == 1
+  return "1 1" if num == 2
+
+  array = [0, 1, 1]
+
+  i = 3
+
+  until i == num + 1
+    current = array[array[i - 1]] + array[i - array[i - 1]]
+  
+    array << current
+
+    i += 1
+  end
+  return array[1..-1].join(' ')
 end
